a275d20f11b04d606437a6849809ffc5004cc425
DanielGonz1 <danielruets@gmail.com>
2023-09-30 00:07:25 -0600

    Se implementaron las nuevas "Excepciones" para un mejor control

--- src/main/java/med/voll/api/infra/errores/TratadorDeErrores.java
+++ src/main/java/med/voll/api/infra/errores/TratadorDeErrores.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ValidationException;
 
 @RestControllerAdvice
 public class TratadorDeErrores {
@@ -21,6 +22,18 @@ public class TratadorDeErrores {
     public ResponseEntity<List<DatosErrorValidacion>> tratarError400(MethodArgumentNotValidException e) {
         var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
         return ResponseEntity.badRequest().body(errores);
+    }
+
+    @ExceptionHandler(ValidacionDeIntegridad.class)
+    public ResponseEntity<String> ErrorHandlerValidacionsDeIntegridad(Exception e) {
+
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> ErrorHandlerValidacionsDeNegocio(Exception e) {
+
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
     private record DatosErrorValidacion(String campo, String error) {
